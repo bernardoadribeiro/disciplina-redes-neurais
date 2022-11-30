ca3b57df81ecdbcbdff2a1c8f17e0289a1c86ec6
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2022-11-30 00:01:33 -0300

    feature: implementa geracao dos graficos de quartis Ex2-b (boxplot)

--- exercicios/lista_2/ex02.py
+++ exercicios/lista_2/ex02.py
@@ -17,34 +17,74 @@ def plot_histogram(data_source, *title):
     
     #plt.show()
 
-def generate_values(lenght, mean, standard_deviation):
+def generate_values(lenght, mean, standard_deviation, order):
     """Generates a random array of a normal distribution"""
-
     array = np.random.normal(loc=mean, scale=standard_deviation, size=lenght)
+
+    if order:
+        return sorted(array)
     return array
 
 def plot_hist_by_mean(mean):
     hist_title = "Media {x}".format(x=mean)
-    value = 10
+    start_value = 10
 
     for i in range(1, 5): # plot 4 hist in 1 figure
-        x = generate_values(value, mean, 1)
+        x = generate_values(start_value, mean, 1)
         plt.subplot(2, 2, i)
         plot_histogram(x, hist_title)
-        value *= 10
+        start_value *= 10
 
     plt.show()
 
 
 # Observações para 10, 100, 1000 e 10000 amostras. Com media=0 // Figure 1
-plot_hist_by_mean(0) 
+# plot_hist_by_mean(0) 
 
 # Observações para 10, 100, 1000 e 10000 amostras. Com media=1 // Figure 2
-plot_hist_by_mean(1) 
+# plot_hist_by_mean(1) 
 
 
 """
     Quartis: Graficos dos quartis da normal e quartis da exponencial
+    A quartiles describe how the data is distributed. It shows the lower (25%), mean and upper values (25%).
+    We will use a boxplot to plot the quartiles graph.
+    Reference: https://mashimo.wordpress.com/2013/07/06/quartiles-and-summary-statistics-in-python/ and https://mashimo.wordpress.com/2013/07/21/visualize-quartiles-and-summary-statistics-in-python/
 """
 
+def plot_quartiles_graph(mean):
+    boxplot_title = "Media {x}".format(x=mean)
+    start_value = 10
 
+    for i in range(1, 5): # plot 4 boxplot in 1 figure
+        x = generate_values(start_value, mean, 1)
+        #plt.subplot(2, 2, i)
+        plt.title(boxplot_title)
+        plt.boxplot(x)
+        start_value *= 10
+        plt.show()
+
+
+# Plot boxplot graph
+quartiles_mean_0 = [
+    generate_values(10,0,1, True), 
+    generate_values(100,0,1, True), 
+    generate_values(1000,0,1, True), 
+    generate_values(10000,0,1, True)
+    ]
+plt.boxplot(quartiles_mean_0, showmeans=True, whis=99)
+plt.title('Mean = 0') # chart title
+plt.xticks([1,2,3,4], ['10','100','1.000', '10.000']) # x axis labels
+plt.show()
+
+quartiles_mean_1 = [
+    generate_values(10,1,1, True),
+    generate_values(100,1,1, True), 
+    generate_values(1000,1,1, True), 
+    generate_values(10000,1,1, True)
+    ]
+plt.boxplot(quartiles_mean_1, showmeans=True, whis=99)
+plt.title('Mean = 1') # chart title
+plt.xticks([1,2,3,4], ['10','100','1.000', '10.000']) # x axis labels
+
+plt.show()
