0877e3eb7a240d34ffafed164e82a05aa6d84509
Bernardo Ribeiro <ribeirobernardo92@gmail.com>
2022-11-30 01:05:44 -0300

    task: add conclusao sobre os quartis + resumo Ex2-b

--- exercicios/lista_2/ex02.py
+++ exercicios/lista_2/ex02.py
@@ -49,7 +49,10 @@ def plot_hist_by_mean(mean):
     Quartis: Graficos dos quartis da normal e quartis da exponencial
     A quartiles describe how the data is distributed. It shows the lower (25%), mean and upper values (25%).
     We will use a boxplot to plot the quartiles graph.
-    Reference: https://mashimo.wordpress.com/2013/07/06/quartiles-and-summary-statistics-in-python/ and https://mashimo.wordpress.com/2013/07/21/visualize-quartiles-and-summary-statistics-in-python/
+    Reference: 
+    - https://mashimo.wordpress.com/2013/07/06/quartiles-and-summary-statistics-in-python/
+    - https://mashimo.wordpress.com/2013/07/21/visualize-quartiles-and-summary-statistics-in-python/
+    - https://fernandafperes.com.br/blog/interpretacao-boxplot/
 """
 
 def plot_quartiles_graph(mean):
@@ -64,6 +67,20 @@ def plot_quartiles_graph(mean):
         start_value *= 10
         plt.show()
 
+def quartile_summary(data):
+    print("\n\n**** Resumo ****")
+    print(f"Media: {np.mean(data):.2}")
+    print(f"Min: {np.min(data):.2}")
+    print(f"Q1 (lower): {np.quantile(data, .25):.2}")
+    print(f"Mediana (Q2): {np.median(data):.2}")
+    print(f"Q3 (upper): {np.quantile(data, .75):.2}")
+    print(f"Max: {np.max(data):.2}")
+    print(f"IRQ (Interquartile Range): {np.quantile(data, .75) - np.quantile(data, .25):.2}")
+
+# Print resume of quantile before plot boxplot
+start_value = 10
+for i in range(4):
+    quartile_summary(generate_values(start_value*10,0,1, True))
 
 # Plot boxplot graph
 quartiles_mean_0 = [
@@ -77,14 +94,29 @@ plt.title('Mean = 0') # chart title
 plt.xticks([1,2,3,4], ['10','100','1.000', '10.000']) # x axis labels
 plt.show()
 
+
+# Print resume of quantile before plot boxplot
+for i in range(4):
+    quartile_summary(generate_values(start_value*10,0,1, True))
+
+# Plot boxplot graph
 quartiles_mean_1 = [
     generate_values(10,1,1, True),
     generate_values(100,1,1, True), 
     generate_values(1000,1,1, True), 
     generate_values(10000,1,1, True)
     ]
-plt.boxplot(quartiles_mean_1, showmeans=True, whis=99)
+plt.boxplot(quartiles_mean_1, showmeans=True, whis=99, showfliers=True)
 plt.title('Mean = 1') # chart title
 plt.xticks([1,2,3,4], ['10','100','1.000', '10.000']) # x axis labels
 
 plt.show()
+
+"""
+    CONCLUSAO
+    - Os valores estão agrupados em torno da média, mesmo que a quantidade de dados aumente;
+    - Não temos outliers;
+    - Há uma distruição simétrica, pois a mediana está no meio dos retangulos.
+    - A amplitude aumenta conforme o número de dados aumenta
+
+"""
